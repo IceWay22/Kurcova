cd46710e7a760aabef3a6e411030ca6c97a8da78
GoaSand <egor.aleksa@bk.ru>
2022-12-27 13:22:03 +0300

    gg2

--- admin/admin.py
+++ admin/admin.py
@@ -30,7 +30,7 @@ def login():
     from dbpri import FDataBase
     dbase = FDataBase(db)
     if 'userlogged' in session:
-        return redirect(url_for('index'))
+        return redirect(url_for('addCommand'))
     elif request.method == 'POST':
         print(dbase.getUser())
         for item in dbase.getUser():
@@ -43,6 +43,23 @@ def login():
             print('Ошибка')
     return render_template('login.html', title='Авторизация', data=dbase.getUser())
 
+
+@admin.route('/comands', methods=["POST", "GET"])
+def addCommand():
+    from main import get_db
+    db = get_db()
+    from dbpri import FDataBase
+    dbase = FDataBase(db)
+    if request.method == 'POST':
+        if len(request.form['name_of_comand']) > 1 and len(request.form['achivements']) > 2 and len(request.form['sostav']) > 2 and len(request.form['identif']) > 2:
+            res = dbase.addCommand(request.form['name_of_comand'], request.form['achivements'], request.form['sostav'], request.form['identif'])
+            if not res:
+                flash('Ошибка добавления', category='error')
+            else:
+                flash('', category='success')
+        else:
+            flash('Ошибка добавления', category='error')
+    return render_template('comands.html', menu=dbase.getMenu())
 
 @admin.route('/logout', methods=['POST', 'GET'])
 def logout():
